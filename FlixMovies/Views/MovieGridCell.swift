dc8b351a08f8af2ae0e920ccdcca9aa423ceb124
akaisani <akaisani@purdue.edu>
2019-01-23 19:18:37 -0500

    Superhero View - created superhero view controller

---
+++ FlixMovies/Views/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  FlixMovies
+//
+//  Created by Abid Amirali on 1/23/19.
+//  Copyright © 2019 Abid Amirali. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
---
+++ SuperheroViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SuperheroViewController.swift
+//  FlixMovies
+//
+//  Created by Abid Amirali on 1/23/19.
+//  Copyright © 2019 Abid Amirali. All rights reserved.
+//
+
+import UIKit
+
+class SuperheroViewController: UIViewController {
+    
+    @IBOutlet weak var moviesCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
+extension SuperheroViewController: UICollectionViewDataSource {
+    
+}
+
+extension SuperheroViewController: UICollectionViewDataSource {
+    
+}
+
+
